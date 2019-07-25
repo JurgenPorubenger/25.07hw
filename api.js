b57f732fee7baf0532005582c3aba7423b0db76a
jurgen <jurgenporubenger@gmail.com>
2019-07-26 01:20:02 +0300

    first

---
+++ api.js
@@ -0,0 +1,9 @@
+let arr = [
+    {name: 'Andrey', surmame: 'Dragon', age:28},
+    {name: 'Feofan', surmame: 'Baklan', age:20},
+    {name: 'Bratan', surmame: 'Urodec', age:16},
+    {name: 'Vorobey', surmame: 'Huev', age:12},
+    {name: 'Vovan', surmame: 'Eblan', age:36},
+    ]
+;
+module.exports = arr;
---
+++ server.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const peoples = require('./api');
+
+let allPeoples = `${JSON.stringify(peoples, null, 2)}`;
+let p1 = `${JSON.stringify(peoples[0])}`;
+let p2 = `${JSON.stringify(peoples[1])}`;
+let p3 = `${JSON.stringify(peoples[2])}`;
+let p4 = `${JSON.stringify(peoples[3])}`;
+let p5 = `${JSON.stringify(peoples[4])}`;
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname,req.url);
+    let contentType = 'text/html';
+
+        switch (path.basename(filePath)) {
+            case '1':
+                res.writeHead(200, {'Content-type': contentType});
+                res.end(p1);
+                break;
+            case '2':
+                res.writeHead(200, {'Content-type': contentType});
+                res.end(p2);
+                break;
+            case '3':
+                res.writeHead(200, {'Content-type': contentType});
+                res.end(p3);
+                break;
+            case '4':
+                res.writeHead(200, {'Content-type': contentType});
+                res.end(p4);
+                break;
+            case '5':
+                res.writeHead(200, {'Content-type': contentType});
+                res.end(p5);
+                break;
+            default:
+                res.writeHead(200, {'Content-type': contentType});
+                res.end(allPeoples);
+        }
+
+    });
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+    console.log('Listening 8080');
+});
+
+//
+
+// fs.appendFile('readme.log', people[0].name +''+'\n', function(error){
+//     if(error) throw error;
+// });
+
